3811b947cd8d4c1a664cb2c8a832cf381c641ded
Xuan Hung <doanxuanhung1997@gmail.com>
2019-06-28 19:20:22 +0700

    edit type phone frontend

--- SPA-Angular-HK-Phone-Store/src/app/type-phone/edit-type-phone/edit-type-phone.component.ts
+++ SPA-Angular-HK-Phone-Store/src/app/type-phone/edit-type-phone/edit-type-phone.component.ts
@@ -1,4 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
+import { TypePhoneService } from 'app/service-server/type-phone.service';
+import { TypePhone } from '../../model-server/type-phone-model';
+// import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { PhoneCompanyService } from 'app/service-server/phone-company.service';
 
 @Component({
   selector: 'app-edit-type-phone',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-type-phone.component.scss']
 })
 export class EditTypePhoneComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  img1: String;
+  img2: String;
+  img3: String;
+  img4: String;
+  img5: String;
+  listPC: any;
+  tp = new TypePhone();
+  constructor(
+    private TypePhoneServive: TypePhoneService,
+    private PCService: PhoneCompanyService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.getID();
+    this.getData();
   }
 
+  getID() {
+    this.route.params.subscribe(params => {
+      this.id = +params['id']; // (+) converts string 'id' to a number
+    })
+  }
+
+  getData() {
+    this.getTypePhone();
+    this.getListPC();
+  }
+
+  getTypePhone() {
+    this.TypePhoneServive.getTypePhone(this.id).subscribe(data => {
+      this.tp = data['typephone'][0];
+      this.img1 = this.tp.tp_img1;
+      this.img2 = this.tp.tp_img2;
+      this.img3 = this.tp.tp_img3;
+      this.img4 = this.tp.tp_img4;
+      this.img5 = this.tp.tp_img5;
+    }, err => {
+
+    })
+  }
+
+  getListPC() {
+    this.PCService.getPhoneCompany().subscribe(data => {
+      this.listPC = data;
+    }, err => {
+
+    })
+  }
 }
