ae2ed796714be082c3df82c4780a070c1a776066
Xuan Hung <doanxuanhung1997@gmail.com>
2019-07-25 00:32:41 +0700

    update mới nhất

--- Frontend-Client-eCommerce/src/app/components/header/header.component.ts
+++ Frontend-Client-eCommerce/src/app/components/header/header.component.ts
@@ -1,37 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import {UpdateviewService} from '../../service/updateview.service';
-import {Router} from '@angular/router';
+import { UpdateviewService } from '../../service/updateview.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  total:number=0;
-  countitem:number=0;
-  search:string;
-  constructor(private udViewService:UpdateviewService,private router:Router) { }
+  total: number = 0;
+  countitem: number = 0;
+  search: string;
+  constructor(private udViewService: UpdateviewService, private router: Router) { }
 
   ngOnInit() {
     this.getTotalCount();
-    this.udViewService.getUpdateView().subscribe((data)=>{
+    this.udViewService.getUpdateView().subscribe((data) => {
       this.getTotalCount();
     });
-    
+
   }
-  getTotalCount(){
-    this.total=0;
-    this.countitem=0;
-    let cart=JSON.parse(localStorage.getItem('cart'));
-    for(var i=0;i<cart.length;i++){
-      let item=JSON.parse(cart[i]);
+  getTotalCount() {
+    this.total = 0;
+    this.countitem = 0;
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    for (var i = 0; i < cart.length; i++) {
+      let item = JSON.parse(cart[i]);
       //console.log("item cart : "+item.tp_name);
-      this.countitem+=item.tp_count*1;
-      this.total+=item.tp_price*item.tp_count;
+      this.countitem += item.tp_count * 1;
+      this.total += item.tp_price * item.tp_count;
     }
   }
-  AddSearch(e){
+  AddSearch(e) {
     e.preventDefault();
-    this.router.navigateByUrl('search/'+this.search);
+    this.router.navigateByUrl('search/' + this.search);
+  }
+
+  getFormatNumber(number) {
+    return (Math.max(0, number).toFixed(0).replace(/(?=(?:\d{3})+$)(?!^)/g, '.'))
   }
 }
